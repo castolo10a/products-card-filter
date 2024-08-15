9518ad85d0d6c8282aa4586c8f30d179422e0101
castolo10a <daniel.302009@hotmail.com>
2024-08-14 22:37:27 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App p-4">
+      <h1 className="text-2xl font-bold text-blue-500">¡Hola, Mundo!</h1>
+    </div>
+  );
+};
+
+export default App;
